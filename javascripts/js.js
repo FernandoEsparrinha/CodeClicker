f9806778cbf79fe390779c2c0862c14fa043d79f
FernandoPinto <nando5000@live.com.pt>
2014-12-04 11:30:48 +0000

    Added new images and working upgrades

--- javascripts/js.js
+++ javascripts/js.js
@@ -5,16 +5,13 @@ var multiplier = 1;
 var cost1 = 5;
 var cost2 = 25;
 var cost3 = 30;
+var cost4 = 60;
 
 function clickKeyboard(){
 	addScore();
 	scoreUpdate();
 
-	if(score >= cost1){
-		document.getElementsByClassName("upgrade")[0].className = "upgrade enabled";
-	} else {
-		document.getElementsByClassName("upgrade")[0].className = "upgrade disabled";
-	}
+	updateAll();
 
 }
 
@@ -33,9 +30,83 @@ function buy1(){
 		multiplier += 3;
 		scoreUpdate();
 		cost1 *= 2;
-		document.getElementById("ug1Cost").innerHTML = "IT Student - \"My favorite programming language its HTML !\" Cost: "+cost1+" LC";	
-	} else {
-		alert("Cant buy it yet !");
-	}
+		document.getElementById("ug1Cost").innerHTML = "IT Student - \"My favorite programming language its HTML !\"<br>Cost: "+cost1+" LC";
+		updateAll();	
+	} 
 	
 };
+
+function buy2(){
+	if(score >= cost2){
+		score -= cost2;
+		multiplier += 6;
+		scoreUpdate();
+		cost2 *= 2;
+		document.getElementById("ug2Cost").innerHTML = "Advanced Developer - He programmed some things in his life.<br>Cost: "+cost2+"LC";
+		updateAll();	
+	} 
+	
+};
+
+function buy3(){
+	if(score >= cost3){
+		score -= cost3;
+		multiplier += 9;
+		scoreUpdate();
+		cost3 *= 3;
+		document.getElementById("ug3Cost").innerHTML = "Nerd - He read all the JQuery documentation.<br>Cost: "+cost3+"LC";
+		updateAll();	
+	} 
+	
+};
+
+function buy4(){
+	if(score >= cost4){
+		score -= cost4;
+		multiplier += 12;
+		scoreUpdate();
+		cost4 *= 4;
+		document.getElementById("ug4Cost").innerHTML = "Crazy Programmer - that crazy guy no one talks to.<br>Cost: "+cost4+"LC";
+		updateAll();	
+	} 
+	
+};
+
+function updateLvl1(){
+	if(score >= cost1){
+		document.getElementsByClassName("upgrade")[0].className = "upgrade enabled";
+	} else  {
+		document.getElementsByClassName("upgrade")[0].className = "upgrade disabled";
+	}
+}
+
+function updateLvl2(){
+	if(score >= cost2){
+		document.getElementsByClassName("upgrade")[1].className = "upgrade enabled";
+	} else {
+		document.getElementsByClassName("upgrade")[1].className = "upgrade disabled";
+	}
+}
+
+function updateLvl3(){
+	if(score >= cost3){
+		document.getElementsByClassName("upgrade")[2].className = "upgrade enabled";
+	} else {
+		document.getElementsByClassName("upgrade")[2].className = "upgrade disabled";
+	}
+}
+
+function updateLvl4(){
+	if(score >= cost4){
+		document.getElementsByClassName("upgrade")[3].className = "upgrade enabled";
+	} else {
+		document.getElementsByClassName("upgrade")[3].className = "upgrade disabled";
+	}
+}
+
+function updateAll(){
+	updateLvl1();
+	updateLvl2();
+	updateLvl3();
+	updateLvl4();
+}
